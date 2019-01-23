bd3340e6e1069f876940fe5f76162880257ea463
YauhenKurlovich <yauhen.kurlovich@leverx.com>
2019-01-23 16:34:51 +0300

    task 2 is done

---
+++ SAPUI5 ODATA/scripts/index.js
@@ -0,0 +1,28 @@
+sap.ui.require([
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/mvc/XMLView",
+	"sap/ui/model/resource/ResourceModel"
+], function (JSONModel, XMLView, ResourceModel) {
+	"use strict";
+
+	// Attach an anonymous function to the SAPUI5 'init' event
+	sap.ui.getCore().attachInit(function () {
+        var url = "https://services.odata.org/V4/TripPinServiceRW/People?$select=FirstName,LastName,Gender";
+		var oModel = new JSONModel(url);
+		sap.ui.getCore().setModel(oModel, "peoples");
+
+		var oResourceModel = new ResourceModel({
+			bundleName: "sap.ui.demo.i18n.i18n"
+		});
+
+		sap.ui.getCore().setModel(oResourceModel, "i18n");
+
+        var oView = new XMLView({
+			viewName: "sap.ui.demo.view.App"
+		}).placeAt("content");
+
+		sap.ui.getCore().getMessageManager().registerObject(oView, true);
+
+		oView.placeAt("content");
+	});
+});
